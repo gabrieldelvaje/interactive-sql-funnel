f23932bd20b689920385697ecee3ecf37a397f84
Gabriel Delvaje Corrêa <81717759+gabrieldelvaje@users.noreply.github.com>
2026-09-22 07:51:11 -0300

    Add project configuration

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const CONFIG = Object.freeze({
+  DATA_MODE: "demo",
+  PARQUET_URL: "./data/ecommerce_events.parquet",
+  MIN_SAMPLE_SIZE: 100,
+  SQL_HIGHLIGHT_MS: 1250,
+  DEMO_SESSIONS: 2400
+});
